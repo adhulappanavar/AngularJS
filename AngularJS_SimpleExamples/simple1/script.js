36d1b02039e7cb35d7c58cbd6859a71bf2d5883c
adhulappanavar <dhulappanavar@yahoo.com>
2014-07-03 19:57:44 +0530

    Simple Examples to learn Scope
    Learn to separate Model data between HTML and JSON Data using Scope.
    Real Simple Examples

---
+++ AngularJS_SimpleExamples/simple1/script.js
@@ -0,0 +1,6 @@
+var main=function($scope) {
+  $scope.message="hello - Simple1";
+  
+    };
+  
+   
---
+++ AngularJS_SimpleExamples/simple2/script.js
@@ -0,0 +1,33 @@
+var main=function($scope) {
+  $scope.message="hello - F1 Driver List";
+  
+  
+    
+  $scope.driversListStatic = [
+      {
+          Driver: {
+              givenName: 'Sebastian',
+              familyName: 'Vettel'
+          },
+          points: 322,
+          nationality: "German",
+          Constructors: [
+              {name: "Red Bull"}
+          ]
+      },
+      {
+          Driver: {
+          givenName: 'Fernando',
+              familyName: 'Alonso'
+          },
+          points: 207,
+          nationality: "Spanish",
+          Constructors: [
+              {name: "Ferrari"}
+          ]
+      }
+    ];
+    
+    };
+  
+   
